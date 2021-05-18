cd487fa9d70debd5a1e6c95086ec041456e4d052
kiloking <kilokingw@gmail.com>
2021-05-18 18:20:08 +0800

    add header

---
+++ src/App/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <section id="header">
+      <div className="caption">
+        <div className="title">Virtual Production</div>
+        <p>
+          Virtual production is the future of filmmaking. With real-time rendered computer graphics and camera tracking technologies, the 3D photo-realistic environment is shown on the LED walls. Backgrounds also move in full perspective with respect to the camera’s position.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Header
