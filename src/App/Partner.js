60afc52a76ade8300a88498e7361b8d3da8a4165
wood.wu <kilokingw@gmail.com>
2022-05-03 22:05:21 +0800

    default lang en

--- src/App/Partner.js
+++ src/App/Partner.js
@@ -14,7 +14,30 @@ function Partner({partnerData}) {
     slidesToShow: 4,
     slidesToScroll: 1,
     nextArrow: <FaChevronRight />,
-    prevArrow: <FaChevronLeft />
+    prevArrow: <FaChevronLeft />,
+    responsive:[
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   };
   return (
     <section id="partner" className='fluid_section'>
--- src/i18n.js
+++ src/i18n.js
@@ -14,7 +14,7 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'zh-TW',             //預設語言
+  lng: 'en',             //預設語言
   fallbackLng: 'zh-TW',     //如果當前切換的語言沒有對應的翻譯則使用這個語言，
   interpolation: {
     escapeValue: false,
