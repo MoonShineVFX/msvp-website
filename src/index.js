78cb54325a7c570f793b4b9b160dfffd79b0e031
wood.wu <kilokingw@gmail.com>
2022-10-03 14:42:29 +0800

    add react-snap for pre render (SEO)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { hydrate, render } from "react-dom";
 import App from './App/App';
 import './index.css'
 import './i18n';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+    hydrate(<App />, rootElement);
+} else {
+    render(<App />, rootElement);
+}
 
