52ec1bef09a91ba1c0cb19293130c00cedd7be5e
wood.wu <kilokingw@gmail.com>
2022-10-04 16:55:27 +0800

    Revert "add react-snap for pre render (SEO)"
    This reverts commit 78cb54325a7c570f793b4b9b160dfffd79b0e031.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import { hydrate, render } from "react-dom";
+import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import './index.css'
 import './i18n';
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement);
-} else {
-    render(<App />, rootElement);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
 
