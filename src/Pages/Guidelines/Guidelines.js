2179c7f985443d68c608ae1af5717fc14b96268a
wood.wu <kilokingw@gmail.com>
2023-04-24 20:16:40 +0800

    Update Guidelines.js

--- src/Pages/Guidelines/Guidelines.js
+++ src/Pages/Guidelines/Guidelines.js
@@ -11,7 +11,10 @@ function Guidelines() {
         className="min-h-[30vh] md:min-h-[65vh] bg-center bg-no-repeat bg-cover relative " 
         style={{backgroundImage: `url(${process.env.PUBLIC_URL +'/images/guidelines/guidelines01.jpg'})`}}
       >
-        <div className=' absolute top-0 left-0  w-full min-h-[30vh] md:min-h-[70vh] min bg-gradient-to-t from-black z-50  '></div>
+        <div 
+          className=' absolute top-0 left-0  w-full min-h-[30vh] md:min-h-[70vh] z-50  '
+          style={{background: "linear-gradient(180deg, rgba(0,0,0,0.0) 70%, rgb(0, 0, 0) 90%)"}}
+          ></div>
       </div>
       <div className='w-10/12 mx-auto '>
         <div className="text-white text-center text-3xl font-bold mb-14 md:mb-28 mt-14 ">{t(`guide_title`)}</div>
