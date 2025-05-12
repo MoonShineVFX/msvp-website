8963da0156c369b6693e44f13f477b24d23fbb67
wood.wu <kilokingw@gmail.com>
2025-05-13 01:39:45 +0800

    up 0513

--- src/App/Space.js
+++ src/App/Space.js
@@ -1,33 +1,38 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next';
-function Space({spaceData,handler}) {
-  const {space} = spaceData
+import React from "react";
+import { useTranslation } from "react-i18next";
+function Space({ spaceData, handler }) {
+  const { space } = spaceData;
   const { t } = useTranslation();
   // 點擊空間
-  const handleClick= (dataId) =>{
-    handler(dataId)
-  }
+  const handleClick = (dataId) => {
+    handler(dataId);
+  };
   return (
     <section id="space">
-      <h1>{t('section_title4')}</h1>
+      <h1>{t("section_title4")}</h1>
       <div className="space_list">
-        {
-          space?
-            space.map((item,index)=>{
-              const {id,title,image}= item
-              return(
-                <div key={id} className="space_item"  onClick={()=>handleClick(id)} data-aos="fade-up">
-                  <div className="space_item_bg" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/images/space/'+ image })`}}></div>
-                  <div className="title" >{t(`${title}`)}</div>
+        {space
+          ? space.map((item, index) => {
+              const { id, title, image } = item;
+              return (
+                <div
+                  key={id}
+                  className="space_item"
+                  onClick={() => handleClick(id)}
+                  data-aos="fade-up"
+                >
+                  <div
+                    className="space_item_bg"
+                    style={{ backgroundImage: `url(${image})` }}
+                  ></div>
+                  <div className="title">{t(`${title}`)}</div>
                 </div>
-              )
-            }) : ""
-        }
-
+              );
+            })
+          : ""}
       </div>
-      
     </section>
-  )
+  );
 }
 
-export default Space
+export default Space;
--- src/App/SpaceSingle.js
+++ src/App/SpaceSingle.js
@@ -1,70 +1,139 @@
-import React,{useState,useEffect} from 'react'
-import { useTranslation } from 'react-i18next';
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { FaTimes } from "react-icons/fa";
-function SpaceSingle({data,handler}) {
-  const {title,link,desc,purpose,spec,spacespce,image,space_image1,spacespec,shooting,ledspec,trussspec,mosys,recommendation } = data
+function SpaceSingle({ data, handler }) {
+  const {
+    title,
+    link,
+    desc,
+    purpose,
+    spec,
+    spacespce,
+    image,
+    link01,
+    link01_img,
+    space_image1,
+    spacespec,
+    shooting,
+    ledspec,
+    trussspec,
+    mosys,
+    rotating,
+    recommendation,
+  } = data;
   // let finalIntro= intro.replace('\\n', '\n')
-  const [active , setActive] = useState(false)
+  const [active, setActive] = useState(false);
   const { t } = useTranslation();
 
   // 點擊按鈕或背景關閉
-  const handleClick= () =>{
-    setActive(!active)
+  const handleClick = () => {
+    setActive(!active);
     setTimeout(() => {
-      handler()
+      handler();
     }, 450);
     // handler()
-  }
+  };
   useEffect(() => {
     const timer = setTimeout(() => {
-      setActive(!active)
+      setActive(!active);
     }, 500);
     return () => clearTimeout(timer);
-  },[]);
+  }, []);
   return (
     <div className="workitem">
-      <div className={active ? "blackbg active" : "blackbg"} onClick={handleClick}></div>
-      <div className={active ? "itemContent img_itemContent active" : "itemContent img_itemContent"}>
-        <div className="closeBtn" onClick={handleClick}><FaTimes /></div>
-        <div className="thumb img_thumb" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/images/space/'+ image })`}}>
+      <div
+        className={active ? "blackbg active" : "blackbg"}
+        onClick={handleClick}
+      ></div>
+      <div
+        className={
+          active
+            ? "itemContent img_itemContent active"
+            : "itemContent img_itemContent"
+        }
+      >
+        <div className="closeBtn" onClick={handleClick}>
+          <FaTimes />
+        </div>
+        <div
+          className="thumb img_thumb"
+          style={{ backgroundImage: `url(${image})` }}
+        >
           <div className="title">{t(`${title}`)}</div>
         </div>
         <article>
           <div className="title">{t(`${title}`)}</div>
           <div className="description">{t(`${desc}`)}</div>
           <div className="space_image">
-            <img src={ space_image1 } alt="" />
+            <img src={space_image1} alt="" />
           </div>
-          <div className="description">
-            <div>{t('spec_desc_title1')}</div>
-            <div dangerouslySetInnerHTML={{ __html:t(`${spacespec}`) }}/>
-          </div>
-          <div className="description">
-            <div>{t('spec_desc_title2')}</div>
-            <div dangerouslySetInnerHTML={{ __html:t(`${shooting}`) }}/>
-          </div>
-          <div className="description">
-            <div>{t('spec_desc_title3')}</div>
-            <div dangerouslySetInnerHTML={{ __html:t(`${ledspec}`) }}/>
-          </div>
-          <div className="description" style={{color:'#8ffff8'}}>
-            <div>{t('spec_desc_title4')}</div>
-            <div dangerouslySetInnerHTML={{ __html:t(`${trussspec}`) }}/>
-          </div>
+          {link01 && (
+            <div className="description">
+              <div>{t("space_link01_title1")}</div>
+              <a
+                href={link01}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="space-link"
+              >
+                <img
+                  src={link01_img}
+                  alt=""
+                  style={{
+                    width: "100%",
+                  }}
+                />
+              </a>
+            </div>
+          )}
+
+          {spacespec && (
+            <div className="description">
+              <div>{t("spec_desc_title1")}</div>
+              <div dangerouslySetInnerHTML={{ __html: t(`${spacespec}`) }} />
+            </div>
+          )}
+
+          {shooting && (
+            <div className="description">
+              <div>{t("spec_desc_title2")}</div>
+              <div dangerouslySetInnerHTML={{ __html: t(`${shooting}`) }} />
+            </div>
+          )}
+          {ledspec && (
+            <div className="description">
+              <div>{t("spec_desc_title3")}</div>
+              <div dangerouslySetInnerHTML={{ __html: t(`${ledspec}`) }} />
+            </div>
+          )}
+          {trussspec && (
+            <div className="description" style={{ color: "#8ffff8" }}>
+              <div>{t("spec_desc_title4")}</div>
+              <div dangerouslySetInnerHTML={{ __html: t(`${trussspec}`) }} />
+            </div>
+          )}
+
+          {rotating && (
+            <div className="description">
+              <div>{t("spec_desc_title7")}</div>
+              <div dangerouslySetInnerHTML={{ __html: t(`${rotating}`) }} />
+            </div>
+          )}
+
           {mosys && (
-            <div className="description" style={{color:'#d8cfad'}}>
-              <div>{t('spec_desc_title6')}</div>
-              <div dangerouslySetInnerHTML={{ __html:t(`${mosys}`) }}/>
+            <div className="description" style={{ color: "#d8cfad" }}>
+              <div>{t("spec_desc_title6")}</div>
+              <div dangerouslySetInnerHTML={{ __html: t(`${mosys}`) }} />
             </div>
           )}
           <div className="description">
-            <div>{t('spec_desc_title5')}</div>
-            <div dangerouslySetInnerHTML={{ __html:t(`${recommendation}`) }}/>
+            <div>{t("spec_desc_title5")}</div>
+            <div dangerouslySetInnerHTML={{ __html: t(`${recommendation}`) }} />
           </div>
         </article>
       </div>
     </div>
-  )
+  );
 }
 
-export default SpaceSingle
+export default SpaceSingle;
