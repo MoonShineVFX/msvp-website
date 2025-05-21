c936fae3d47a14a3c82ecd3b72d7f947a968272e
wood.wu <kilokingw@gmail.com>
2025-05-21 17:45:37 +0800

    up mobile navbar

--- src/Components/MobilePagesNavbar.js
+++ src/Components/MobilePagesNavbar.js
@@ -1,48 +1,72 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next';
-import { Link ,useLocation  } from "react-router-dom";
-function MobilePagesNavbar({data,isToggled,switchToggle}) {
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link, useLocation } from "react-router-dom";
+function MobilePagesNavbar({ data, isToggled, switchToggle }) {
   const { t, i18n } = useTranslation();
   const changeLanguage = (lng) => {
-      i18n.changeLanguage(lng);
+    i18n.changeLanguage(lng);
   };
   const handleLinkClick = (event) => {
-    switchToggle()
-
+    switchToggle();
   };
+  const scrollToElement = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  const { pathname } = useLocation();
   return (
-    <div className={isToggled ?  "mobile_navlist active" : "mobile_navlist"}>
-        <ul>
-          { data?
-            data.map((item,index)=>{
-              if(item.type === 'about'){
-                return     (
-                  <li key='about'>
-                    <a 
-                      href="https://moonshine.tw/about"
-                      target="_blank"
-                    >
-                      {t(`${item.chtName}`)}
-                    </a>
-                  </li>
-                )            
-              }
-              return(
-                <li key={index}>
-                  <Link 
-                    to={item.type}
-                    onClick={handleLinkClick}
-                  >
-                    {t(`${item.chtName}`)}
-                  </Link>
-                </li>
-              )
-            }): ""
-          }
-
-        </ul>
+    <div className={isToggled ? "mobile_navlist active" : "mobile_navlist"}>
+      <ul>
+        {data &&
+          data.map((item, index) => (
+            <li key={index}>
+              {(() => {
+                switch (item.type) {
+                  case "external":
+                    return (
+                      <a
+                        href={item.href}
+                        target="_blank"
+                        className={
+                          pathname.substring(1) === item.type ? "active" : ""
+                        }
+                      >
+                        {t(`${item.chtName}`)}
+                      </a>
+                    );
+                  case "internal":
+                    return (
+                      <Link
+                        to={item.href}
+                        className={
+                          pathname.substring(1) === item.type ? "active" : ""
+                        }
+                      >
+                        {t(`${item.chtName}`)}
+                      </Link>
+                    );
+                  case "scroll":
+                    return (
+                      <div
+                        onClick={() => scrollToElement(item.href)}
+                        className={
+                          pathname.substring(1) === item.type ? "active" : ""
+                        }
+                      >
+                        {t(`${item.chtName}`)}
+                      </div>
+                    );
+                  default:
+                    return null;
+                }
+              })()}
+            </li>
+          ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default MobilePagesNavbar
+export default MobilePagesNavbar;
--- src/Components/PagesNavbar.js
+++ src/Components/PagesNavbar.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 import { FaBars } from "react-icons/fa";
-import { useTranslation } from 'react-i18next';
-import { Link ,useLocation  } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Link, useLocation } from "react-router-dom";
 
-function PagesNavbar({data ,toggleTrueFalse}) {
+function PagesNavbar({ data, toggleTrueFalse }) {
   const { t, i18n } = useTranslation();
   const changeLanguage = (lng) => {
-      i18n.changeLanguage(lng);
+    i18n.changeLanguage(lng);
   };
   const scrollToElement = (id) => {
     const element = document.getElementById(id);
@@ -15,86 +15,89 @@ function PagesNavbar({data ,toggleTrueFalse}) {
     }
   };
   const { pathname } = useLocation();
-  console.log(pathname)
   return (
-    <div id="navbar" className='site-menu'>
+    <div id="navbar" className="site-menu">
       <div className="logo">
-        <Link
-          to={'/'}
-        >
-          <img src={process.env.PUBLIC_URL+'/images/2022/MSXR_White_H.png'} alt="" />
+        <Link to={"/"}>
+          <img
+            src={process.env.PUBLIC_URL + "/images/2022/MSXR_White_H.png"}
+            alt=""
+          />
         </Link>
-        
       </div>
       <div className="navlist">
         <ul>
-        {data && data.map((item, index) => (
-          <li key={index}>
-            {(() => {
-              switch (item.type) {
-                case 'external':
-                  return (
-                    <a
-                      href={item.href}
-                      target="_blank"
-                      className={pathname.substring(1) === item.type ? 'active' : ''}
-                    >
-                      {t(`${item.chtName}`)}
-                    </a>
-                  );
-                case 'internal':
-                  return (
-                    <Link
-                      to={item.href}
-                      className={pathname.substring(1) === item.type ? 'active' : ''}
-                    >
-                      {t(`${item.chtName}`)}
-                    </Link>
-                  );
-                case 'scroll':
-                  return (
-                    <div
-                      onClick={() => scrollToElement(item.href)}
-                      className={pathname.substring(1) === item.type ? 'active' : ''}
-                    >
-                      {t(`${item.chtName}`)}
-                    </div>
-                  );
-                default:
-                  return null;
-              }
-            })()}
-          </li>
-        ))}
+          {data &&
+            data.map((item, index) => (
+              <li key={index}>
+                {(() => {
+                  switch (item.type) {
+                    case "external":
+                      return (
+                        <a
+                          href={item.href}
+                          target="_blank"
+                          className={
+                            pathname.substring(1) === item.type ? "active" : ""
+                          }
+                        >
+                          {t(`${item.chtName}`)}
+                        </a>
+                      );
+                    case "internal":
+                      return (
+                        <Link
+                          to={item.href}
+                          className={
+                            pathname.substring(1) === item.type ? "active" : ""
+                          }
+                        >
+                          {t(`${item.chtName}`)}
+                        </Link>
+                      );
+                    case "scroll":
+                      return (
+                        <div
+                          onClick={() => scrollToElement(item.href)}
+                          className={
+                            pathname.substring(1) === item.type ? "active" : ""
+                          }
+                        >
+                          {t(`${item.chtName}`)}
+                        </div>
+                      );
+                    default:
+                      return null;
+                  }
+                })()}
+              </li>
+            ))}
 
-          <div className='lang_btngroup'>
-            <li className={i18n.language === 'zh-TW' ? 'active' : ''}>
+          <div className="lang_btngroup">
+            <li className={i18n.language === "zh-TW" ? "active" : ""}>
               <a onClick={() => changeLanguage("zh-TW")}>中文</a>
             </li>
-            <li className='divider'>/</li>
-            <li className={i18n.language === 'en' ? 'active' : ''}>
-                <a onClick={() => changeLanguage("en")}>ENG</a>
+            <li className="divider">/</li>
+            <li className={i18n.language === "en" ? "active" : ""}>
+              <a onClick={() => changeLanguage("en")}>ENG</a>
             </li>
           </div>
-
-          
         </ul>
       </div>
 
-      <div className="mobileNavBtn" >
-        <div className={i18n.language === 'zh-TW' ? 'active' : ''}>
+      <div className="mobileNavBtn">
+        <div className={i18n.language === "zh-TW" ? "active" : ""}>
           <a onClick={() => changeLanguage("zh-TW")}>中文</a>
         </div>
-        <div className={i18n.language === 'en' ? 'active' : ''}>
+        <div className={i18n.language === "en" ? "active" : ""}>
           <a onClick={() => changeLanguage("en")}>ENG</a>
         </div>
         <div onClick={toggleTrueFalse}>
-          <FaBars/>
+          <FaBars />
         </div>
-          
       </div>
     </div>
-  )
+  );
 }
 
-export default PagesNavbar
+export default PagesNavbar;
